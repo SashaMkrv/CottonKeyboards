c8fb09e1e31772eebe62c792fbd5b6c9397fdfac
ScreechNothing <sasha.makarava@gmail.com>
2017-05-09 14:15:47 -0400

    Initial commit

---
+++ app/src/main/java/shop/cottonkeyboards/MainActivity.java
@@ -0,0 +1,20 @@
+package shop.cottonkeyboards;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://shopicruit.myshopify.com/admin/orders.json?page=1&access_token=c32313df0d0ef512ca64d5b336a0d7c6";
+        
+
+    }
+}
